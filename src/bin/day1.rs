db60b5781a7fa5df4dc1da9af54c29c51b5a4570
Simon Siegler <simon@simon-siegler.de>
2020-12-01 08:31:43 +0100

    Solve day 1 part 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,43 @@
+use std::fs::{File};
+use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+
+const TARGET_SUM: i32 = 2020;
+
+fn main() {
+    let input = File::open("inputs/day1.txt").expect("Failed to open input file");
+    let input = BufReader::new(input)
+        .lines().map(|line| line.expect("Failed to read line").parse::<i32>().expect("failed to parse line as number"));
+    let (a, b) = find_first_pair_with_sum(input, TARGET_SUM).expect("No pair found");
+    println!("{} * {} = {}", a, b, a * b);
+}
+
+fn find_first_pair_with_sum<I>(numbers: I, sum: i32) -> Option<(i32, i32)>
+    where I: Iterator<Item=i32> {
+    let mut seen = HashSet::new();
+    for number in numbers {
+        let partner = sum - number;
+        if seen.contains(&partner) {
+            return Some((partner, number));
+        }
+        seen.insert(number);
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn finds_first_pair_for_sum() {
+        let numbers = vec![1721,
+                           979,
+                           366,
+                           299,
+                           675,
+                           1456];
+        assert_eq!(find_first_pair_with_sum(numbers.into_iter(), TARGET_SUM), Some((1721, 299)));
+    }
+}
+
