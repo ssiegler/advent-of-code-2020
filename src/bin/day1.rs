820a8cdd8567bdc258fe970f695d46e891d7bd74
Simon Siegler <simon@simon-siegler.de>
2020-12-01 10:54:42 +0100

    Extract reading numbers into lib.rs

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,14 +1,12 @@
-use std::fs::{File};
-use std::io::{BufRead, BufReader};
 use std::collections::HashSet;
+use advent_of_code::read_numbers_from_file;
+use std::path::Path;
 
 const TARGET_SUM: i32 = 2020;
 
 fn main() {
-    let input = File::open("inputs/day1.txt").expect("Failed to open input file");
-    let input : Vec<i32> = BufReader::new(input)
-        .lines().map(|line| line.expect("Failed to read line").parse::<i32>().expect("failed to parse line as number")).collect();
-    let (a, b) = find_first_pair_with_sum(input.as_slice(), TARGET_SUM).expect("No pair found");
+    let input = read_numbers_from_file(Path::new("inputs/day1.txt"));
+    let (a, b) = find_first_pair_with_sum(&input, TARGET_SUM).expect("No pair found");
     println!("{} * {} = {}", a, b, a * b);
     let (a, b, c) = find_first_triplet_with_sum(&input, TARGET_SUM).expect("No triplet found");
     println!("{} * {} * {} = {}", a, b, c, a*b*c);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::path::Path;
+
+pub fn read_numbers_from_file(path: &Path) -> Vec<i32> {
+    let input = File::open(path).expect("Failed to open input file");
+    let input: Vec<i32> = BufReader::new(input)
+        .lines()
+        .map(|line|
+            line.expect("Failed to read line"))
+        .filter(|line| !line.is_empty())
+        .map(|line| line.parse::<i32>().unwrap_or_else(|_| panic!("failed to parse line as number: {:?}", &line)))
+        .collect();
+    input
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::{Write};
+    use tempfile::NamedTempFile;
+    use crate::read_numbers_from_file;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn reads_numbers_from_file() -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = NamedTempFile::new()?;
+        writeln!(file, "1
+2
+12
+13
+{}
+1000
+-1
+{}
+", i32::max_value(), i32::min_value())?;
+
+        assert_eq!(read_numbers_from_file(file.path()), &[1, 2, 12, 13, i32::max_value(), 1000, -1, i32::min_value()]);
+
+        Ok(())
     }
 }
