6fd0eaef674ab96ed5c41b834f8c0a3025436ed0
Simon Siegler <simon@simon-siegler.de>
2020-12-03 08:11:10 +0100

    Solve day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,11 +1,23 @@
 use advent_of_code::read_lines;
 
+const SLOPES: &[Slope] = &[
+    Slope { right: 1, down: 1 },
+    Slope { right: 3, down: 1 },
+    Slope { right: 5, down: 1 },
+    Slope { right: 7, down: 1 },
+    Slope { right: 1, down: 2 },
+];
+
 fn main() {
     let tree_count: usize = count_trees(
         read_lines("inputs/day3.txt".as_ref()),
         &Slope { right: 3, down: 1 },
     );
-    println!("Encountered {} trees", tree_count)
+    println!("Encountered {} trees", tree_count);
+    let answer: usize = SLOPES.iter().map(|slope| count_trees(
+        read_lines("inputs/day3.txt".as_ref()),
+        slope)).product();
+    println!("Product of encountered trees: {}", answer);
 }
 
 fn count_trees<T>(tree_rows: impl Iterator<Item = T>, slope: &Slope) -> usize
@@ -52,5 +64,14 @@ mod tests {
     fn encountered_7_trees_in_example_with_slope_3_right_1_down() {
         let tree_count = count_trees(EXAMPLE_INPUT.lines(), &Slope { right: 3, down: 1 });
         assert_eq!(tree_count, 7);
+    }
+
+    #[test]
+    fn encountered_trees_with_scopes_in_example() {
+        let tree_counts: Vec<usize> = SLOPES
+            .iter()
+            .map(|slope| count_trees(EXAMPLE_INPUT.lines(), slope))
+            .collect();
+        assert_eq!(tree_counts, &[2, 7, 3, 4, 2]);
     }
 }
