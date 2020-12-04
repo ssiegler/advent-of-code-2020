c440af99996499070ae5dbc6961f8291b3a4f752
Simon Siegler <simon@simon-siegler.de>
2020-12-04 08:46:40 +0100

    Clarify validity concepts

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -3,9 +3,9 @@ use std::collections::HashMap;
 fn main() {
     let valid_count =
         read_passports(&std::fs::read_to_string("inputs/day4.txt").expect("Failed to read input"))
-            .filter(is_valid)
+            .filter(has_required_fields)
             .count();
-    println!("Got {} valid passports", valid_count);
+    println!("Got {} passports with all required fields", valid_count);
 }
 
 struct Passport<'a> {
@@ -38,13 +38,26 @@ const REQUIRED_KEYS: &[&str] = &[
     "hcl", // (Hair Color)
     "ecl", // (Eye Color)
     "pid", // (Passport ID)
-           // "cid", // (Country ID)
 ];
 
-fn is_valid(passport: &Passport) -> bool {
+fn has_required_fields(passport: &Passport) -> bool {
     REQUIRED_KEYS
         .iter()
         .all(|key| passport.fields.contains_key(key))
+}
+
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+// hgt (Height) - a number followed by either cm or in:
+// If cm, the number must be at least 150 and at most 193.
+// If in, the number must be at least 59 and at most 76.
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+// cid (Country ID) - ignored, missing or not.
+fn is_valid(passport: &Passport) -> bool {
+    todo!()
 }
 
 #[cfg(test)]
@@ -67,6 +80,11 @@ iyr:2011 ecl:brn hgt:59in";
 
     #[test]
     fn reports_2_valid_passports_in_example() {
-        assert_eq!(2, read_passports(EXAMPLE_INPUT).filter(is_valid).count());
+        assert_eq!(
+            2,
+            read_passports(EXAMPLE_INPUT)
+                .filter(has_required_fields)
+                .count()
+        );
     }
 }
