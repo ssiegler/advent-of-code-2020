6be0231b373edb0a0cf6c861d363b7fb52ed07d9
Simon Siegler <simon@simon-siegler.de>
2020-12-08 21:07:13 +0100

    Convert day03 from binary to module

---
+++ src/day03.rs
@@ -0,0 +1,54 @@
+use crate::puzzle::{Input, Puzzle};
+
+type Day03 = Input<Vec<String>>;
+
+struct Slope {
+    down: usize,
+    right: usize,
+}
+
+impl Day03 {
+    fn count_trees(&self, slope: &Slope) -> usize {
+        self.iter()
+            .step_by(slope.down)
+            .enumerate()
+            .filter(|(step, row)| row.chars().cycle().nth(step * slope.right).unwrap() == '#')
+            .count()
+    }
+}
+
+impl Puzzle for Day03 {
+    fn solve_part1(&self) -> String {
+        self.count_trees(&Slope { down: 1, right: 3 }).to_string()
+    }
+
+    fn solve_part2(&self) -> String {
+        [
+            Slope { right: 1, down: 1 },
+            Slope { right: 3, down: 1 },
+            Slope { right: 5, down: 1 },
+            Slope { right: 7, down: 1 },
+            Slope { right: 1, down: 2 },
+        ]
+        .iter()
+        .map(|slope| self.count_trees(&slope))
+        .product::<usize>()
+        .to_string()
+    }
+}
+
+test_puzzle!(Day03;
+    Example("\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#", 7, 336),
+    File("inputs/day03.txt", 234, 5813773056usize)
+);
